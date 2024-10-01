7516d6c8c86866b5676b36448d39180fb95dacdd
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-10-01 06:05:06 -0700

    Create ytDownloader.py

---
+++ YoutubeVideoDownloader/ytDownloader.py
@@ -0,0 +1,12 @@
+#dependencies: pip install pytubefix
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+ 
+url = input("Enter the link of the Youtube video: ")
+ 
+yt = YouTube(url, on_progress_callback = on_progress)
+print(yt.title)
+ 
+ys = yt.streams.get_highest_resolution()
+ys.download()
+print("Downloaded")
