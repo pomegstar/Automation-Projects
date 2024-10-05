d3d0ac72f8e6e190aa49c06069bef78097a095de
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-10-05 09:03:09 -0700

    Update txtToMp3.py

--- AudioBook/txtToMp3.py
+++ AudioBook/txtToMp3.py
@@ -1,34 +1,36 @@
 import pyttsx3
-fname = input('Enter the pdf file name with the path: ')
+import PyPDF2
+#fname = input('Enter the txt file name with the path: ')
 speaker = pyttsx3.init()
 
+# # pdf to speech
+fname = input('Enter the pdf file name with the path: ')
+startp = int(input("Give the start page number: "))
+endp = int(input("Give the end page number: "))
+book = open(fname, 'rb')
+pdfReader = PyPDF2.PdfReader(book)
+pages = len(pdfReader.pages)
 
-# .pdf to .mp3
-
-# import PyPDF2
-# startp = int(input("Which pages do you wanna listen?: "))
-# endp = int(input("Give the end page: "))
-# book = open(fname, 'rb')
-# pdfReader = PyPDF2.PdfReader(book)
-# pages = len(pdfReader.pages)
-
-# for num in range(startp, endp):
-#     page = pdfReader.pages[num]
-#     text = page.extract_text()
-#     # speaker.say(text)
-#     speaker.save_to_file(text, 'C:\Users\Nazmul\Documents\jtest.mp3')
-#     speaker.runAndWait()
+for num in range(startp, endp):
+    page = pdfReader.pages[num]
+    text = page.extract_text()
+    # speaker.say(text)
+    rate = speaker.getProperty('rate')
+    speaker.setProperty('rate', rate-20)
+    speaker.save_to_file(text, r'E:\Nazmul\Music\pdfTest.mp3')
+    speaker.runAndWait()
+print(f'Successfully converted from PDF to speech')
 
 
+# # Text file (.txt) to speech (.mp3)
 
-# Text file (.txt) to speech (.mp3)
-
-with open(fname, 'r', encoding='utf-8') as file:
-        txt = file.read()
-rate = speaker.getProperty('rate')
-speaker.setProperty('rate', rate-20)
-speaker.save_to_file(txt, 'test.mp3')
-speaker.runAndWait()
+# with open(fname, 'r', encoding='utf-8') as file:
+#         txt = file.read()
+# rate = speaker.getProperty('rate')
+# speaker.setProperty('rate', rate-20)
+# speaker.save_to_file(txt, r'E:\Nazmul\Music\txtTest.mp3')
+# speaker.runAndWait()
+# print(f'Successfully converted from text to speech')
 
 #speed_test
 # rate = speaker.getProperty('rate')
